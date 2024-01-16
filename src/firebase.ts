c21b6913b0ad5aca229dfc4b03607b2d2c134a77
hyunjun <khjae0207@naver.com>
2024-01-16 10:45:47 +0900

    Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHTKo6WaCCynbwQb2nR2P2bvVtDGR4sGc",
+  authDomain: "i-llloveidol.firebaseapp.com",
+  projectId: "i-llloveidol",
+  storageBucket: "i-llloveidol.appspot.com",
+  messagingSenderId: "15508393935",
+  appId: "1:15508393935:web:fdf745cb7fe0bd3e24ccba"
+};
+
+const app = initializeApp(firebaseConfig);
