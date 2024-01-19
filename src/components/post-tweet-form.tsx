c0e00c19943a23dbe2a33c91cc7c04b4420dc4a8
hyunjun <khjae0207@naver.com>
2024-01-19 11:23:46 +0900

    Tweeting to Firestore

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from "firebase/firestore";
 import { useState } from "react";
 import styled from "styled-components";
+import { auth, db } from "../firebase";
 
 const Form=styled.form`
     display:flex;
@@ -16,9 +18,9 @@ const TextArea=styled.textarea`
     background-color:black;
     width:100%;
     resize:none;
+    font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     &::placeholder {
         font-size:16px;
-        font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
     &:focus {
         outline:none;
@@ -68,8 +70,28 @@ export default function PostTweetForm() {
             setFile(files[0]);
         }
     };
+    const onSubmit=async(e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const user=auth.currentUser;
+        if(!user||isLoading||tweet===""||tweet.length>180) return;
+
+        try {
+            setLoading(true);
+            await addDoc(collection(db,"tweets"), {
+                tweet,
+                createdAt:Date.now(),
+                username:user.displayName || "Anonymous",
+                userId:user.uid,
+            })
+        } catch(e) {
+            console.log(e);
+        } finally {
+            setLoading(false);
+        }
+
+    }
     return (
-        <Form>
+        <Form onSubmit={onSubmit}>
             <TextArea rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?!"/>
             <AttachFileButton htmlFor="file">{file ? "Photo added ✅":"Add photo"}</AttachFileButton>
             <AttachFileInput onChange={onFileChange} type="file" id="file" accept="image/*"/>
